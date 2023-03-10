ab91baf77c5ff1acfc8267669d0be76e415e8bf5
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2023-03-09 23:04:21 -0300

    feat: add last center models

--- src/assets/center_tiles/index.js
+++ src/assets/center_tiles/index.js
@@ -37,6 +37,26 @@ import LH3042 from './lh_3042.svg';
 import LH3043 from './lh_3043.svg';
 import LH3044 from './lh_3044.svg';
 import LH3045 from './lh_3045.svg';
+import LH3046 from './lh_3046.svg';
+import LH3048 from './lh_3048.svg';
+import LH3052 from './lh_3052.svg';
+import LH3053 from './lh_3053.svg';
+import LH3054 from './lh_3054.svg';
+import LH3055 from './lh_3055.svg';
+import LH3057 from './lh_3057.svg';
+import LH3058 from './lh_3058.svg';
+import LH3059 from './lh_3059.svg';
+import LH3060 from './lh_3060.svg';
+import LH3061 from './lh_3061.svg';
+import LH3062 from './lh_3062.svg';
+import LH3063 from './lh_3063.svg';
+import LH3064 from './lh_3064.svg';
+import LH3070 from './lh_3070.svg';
+import LH3071 from './lh_3071.svg';
+import LH3072 from './lh_3072.svg';
+import LH3100 from './lh_3100.svg';
+import LH3101 from './lh_3101.svg';
+import LH3102 from './lh_3102.svg';
 
 // TILES HAVE THREE MAIN TYPES:
 // 1. NORMAL: NO SEQUENCE, SIDE BY SIDE IS CORRECT
@@ -83,6 +103,26 @@ const tiles = [
     {img: LH3043, code: 'LH 3043', type: 'REFLECTIVE', size: 20, numColors: 3},
     {img: LH3044, code: 'LH 3044', type: 'NORMAL', size: 20, numColors: 4},
     {img: LH3045, code: 'LH 3045', type: 'NORMAL', size: 20, numColors: 2},
+    {img: LH3046, code: 'LH 3046', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3048, code: 'LH 3048', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3052, code: 'LH 3052', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3053, code: 'LH 3053', type: 'REFLECTIVE', size: 20, numColors: 3},
+    {img: LH3054, code: 'LH 3054', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3055, code: 'LH 3055', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3057, code: 'LH 3057', type: 'NORMAL', size: 20, numColors: 4},
+    {img: LH3058, code: 'LH 3058', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3059, code: 'LH 3059', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3060, code: 'LH 3060', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3061, code: 'LH 3061', type: 'REFLECTIVE', size: 20, numColors: 3},
+    {img: LH3062, code: 'LH 3062', type: 'ROTATION', size: 20, numColors: 3},
+    {img: LH3063, code: 'LH 3063', type: 'NORMAL', size: 20, numColors: 2},
+    {img: LH3064, code: 'LH 3064', type: 'REFLECTIVE', size: 20, numColors: 2},
+    {img: LH3070, code: 'LH 3070', type: 'NORMAL', size: 20, numColors: 3},
+    {img: LH3071, code: 'LH 3071', type: 'REFLECTIVE', size: 20, numColors: 2},
+    {img: LH3072, code: 'LH 3072', type: 'NORMAL', size: 20, numColors: 2},
+    {img: LH3100, code: 'LH 3100', type: 'REFLECTIVE', size: 20, numColors: 3},
+    {img: LH3101, code: 'LH 3101', type: 'NORMAL', size: 20, numColors: 4},
+    {img: LH3102, code: 'LH 3102', type: 'REFLECTIVE', size: 20, numColors: 4},
 ];
 
 export default tiles;
