e25677fc6b602f3ad827b1d0dcaa7961a7a5e5b1
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2023-03-11 17:09:32 -0300

    feat: add edge tiles

--- src/assets/edge_tiles/index.js
+++ src/assets/edge_tiles/index.js
@@ -1,8 +1,27 @@
-import LH3078 from './lh_3078.svg';
-import LH3071 from './lh_3071.svg';
+import LH3065 from './lh_3065.svg';
+import LH3066 from './lh_3066.svg';
+import LH3069 from './lh_3069.svg';
+import LH3073 from './lh_3073.svg';
+import LH3075 from './lh_3075.svg';
+import LH3076 from './lh_3076.svg';
+import LH3077 from './lh_3077.svg';
+import LH3090 from './lh_3090.svg';
+import LH3106 from './lh_3106.svg';
+import LH3159 from './lh_3159.svg';
+import LH3187 from './lh_3187.svg';
 
 const tiles = [
-    {img: LH3078, code: 'LH 3078', size: 20, numColors: 2, cornerModel: { img: LH3071, numColors: 2}},
+    {img: LH3065, code: 'LH 3065', size: 20, numColors: 4, cornerModel: { img: LH3065, numColors: 4}},
+    {img: LH3066, code: 'LH 3066', size: 20, numColors: 4, cornerModel: { img: LH3066, numColors: 4}},
+    {img: LH3069, code: 'LH 3069', size: 20, numColors: 3, cornerModel: { img: LH3069, numColors: 3}},
+    {img: LH3073, code: 'LH 3073', size: 20, numColors: 3, cornerModel: { img: LH3073, numColors: 3}},
+    {img: LH3075, code: 'LH 3075', size: 20, numColors: 3, cornerModel: { img: LH3075, numColors: 3}},
+    {img: LH3076, code: 'LH 3076', size: 20, numColors: 3, cornerModel: { img: LH3076, numColors: 3}},
+    {img: LH3077, code: 'LH 3077', size: 20, numColors: 2, cornerModel: { img: LH3077, numColors: 2}},
+    {img: LH3090, code: 'LH 3090', size: 20, numColors: 5, cornerModel: { img: LH3090, numColors: 5}},
+    {img: LH3106, code: 'LH 3106', size: 20, numColors: 3, cornerModel: { img: LH3106, numColors: 3}},
+    {img: LH3159, code: 'LH 3159', size: 20, numColors: 4, cornerModel: { img: LH3159, numColors: 3}},
+    {img: LH3187, code: 'LH 3187', size: 20, numColors: 3, cornerModel: { img: LH3187, numColors: 3}},
 ];
 
 export default tiles;
